a91f9234b1123bbc26a0d70278ca8e751eb04d49
Gautam <gautam.kmr2893@outlook.com>
2022-07-24 14:07:14 +0530

    final_code

--- leet_code1.py
+++ leet_code1.py
@@ -1,18 +1,21 @@
 from typing import List
 
+class Missing_Number_Solution:
+    def __init__(self,list_nums) -> None:
+        self.nums=list_nums
+        pass
 
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        occurences = [0, ] * (n + 1)
-        for num in nums:
-            occurences[num] = 1
+    def missingNumber(self, given_list: List[int]) -> int:
+        n = len(given_list)
+        number_occurance = [0, ] * (n + 1)
+        for num in given_list:
+            number_occurance[num] = 1
 
-        for i, occur in enumerate(occurences):
+        for i, occur in enumerate(number_occurance):
             if occur == 0:
                 return i
 
+mylist=[1,0,3,5,2]
+sol = Missing_Number_Solution(mylist)
+print(sol.missingNumber(mylist))
 
-class Solution2:
-    def missingNumber(self, nums: List[int]) -> int:
-        return sum(range(0, len(nums) + 1)) - sum(nums)
